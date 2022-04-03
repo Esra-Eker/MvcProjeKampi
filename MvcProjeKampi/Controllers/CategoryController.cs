95eb754bb7ebc142da38f8f2d6317b29279db8bb
Esra <elifesraeker@gmail.com>
2022-04-03 22:40:36 +0300

    Yeni kategori eklendi, HttpGet ve HttpPost attributeları eklendi

--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,19 @@ namespace MvcProjeKampi.Controllers
         {
             var categoryvalues = cm.GetAllBL(); //categoryvalues isimli değişkenimin içine category tablomda yer alan değerler gelecek.
             return View(categoryvalues);
+        }
+
+        [HttpGet] //sayfa yüklendiği zaman çalışacak.
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]  //AddCategory metoduna HttpPost attribute u devreye girdiği zaman çalışacaksın dedim. yani sayfamda bir şey post edildiği zaman çalışacak. HttpGet i de yukarıda yazdım, sebebi: sayfa yüklenir yüklenmez veri kaydetme işlemi yapmamak. 
+        public ActionResult AddCategory(Category p)
+        {
+            cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList"); //ekleme işlemini gerçekleştirdikten sonra beni GetCategoryList e yönlendir.
         }
 
     }
