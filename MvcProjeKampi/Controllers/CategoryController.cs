f6f22b9d4a246c582f259d79aa3849b429f000d0
Esra <elifesraeker@gmail.com>
2022-04-05 23:18:58 +0300

    Validasyon işlemi yapıldı

--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,7 +36,21 @@ namespace MvcProjeKampi.Controllers
         public ActionResult AddCategory(Category p)
         {
             //cm.CategoryAddBL(p);
-            return RedirectToAction("GetCategoryList"); //ekleme işlemini gerçekleştirdikten sonra beni GetCategoryList e yönlendir.
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(p);
+            if(results.IsValid)
+            {
+                cm.CategoryAddBL(p);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View(); 
         }
 
     }
