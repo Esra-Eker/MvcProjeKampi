e3a4665ef091321b4c3a650f200a21a7c28b1129
Esra <elifesraeker@gmail.com>
2022-04-13 21:37:44 +0300

    Başlık işlemlerinde DropDownList ve ilişki kullanımı

--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,30 @@ namespace MvcProjeKampi.Controllers
     public class HeadingController : Controller
     {
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
             var headingvalues = hm.GetList();
             return View(headingvalues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;  //yaptığım işlemlerini View tarafına taşıyorum böylece.
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAddBL(p);
+            return RedirectToAction("Index");
         }
     }
 }
