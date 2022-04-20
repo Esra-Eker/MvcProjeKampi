44bbe0852aa7f3d24a65f85835ebd0ade15bea41
Esra <elifesraeker@gmail.com>
2022-04-20 23:30:24 +0300

    AboutPartial eklendi

--- MvcProjeKampi/Controllers/AboutController.cs
+++ MvcProjeKampi/Controllers/AboutController.cs
@@ -31,5 +31,10 @@ namespace MvcProjeKampi.Controllers
             abm.AboutAdd(p);
             return RedirectToAction("Index");
         }
+
+        public PartialViewResult AboutPartial()
+        {
+            return PartialView();
+        }
     }
 }
