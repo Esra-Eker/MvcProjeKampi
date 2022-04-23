6e5d261f0af237a54b307b003d122b665e0e457c
Esra <elifesraeker@gmail.com>
2022-04-23 15:14:56 +0300

    Yeni mesaj sayfası

--- BusinessLayer/Abstract/IMessageService.cs
+++ BusinessLayer/Abstract/IMessageService.cs
@@ -9,7 +9,8 @@ namespace BusinessLayer.Abstract
 {
     interface IMessageService
     {
-        List<Message> GetList();
+        List<Message> GetListInbox();
+        List<Message> GetListSendbox();
         void MessageAdd(Message message);
         Message GetByID(int id);
         void MessageDelete(Message message);
--- MvcProjeKampi/Controllers/MessageController.cs
+++ MvcProjeKampi/Controllers/MessageController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,8 +16,24 @@ namespace MvcProjeKampi.Controllers
         MessageManager mm = new MessageManager(new EfMessageDal());
         public ActionResult Inbox()
         {
-            var messagelist = mm.GetList();
+            var messagelist = mm.GetListInbox();
             return View(messagelist);
         }
+        public ActionResult Sendbox()
+        {
+            var messagelist = mm.GetListSendbox();
+            return View(messagelist);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            return View();
+        }
+
     }
 }
