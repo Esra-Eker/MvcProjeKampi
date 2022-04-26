603914451b3c2a8cd229b9c0074ce2a42cbf1233
Esra <elifesraeker@gmail.com>
2022-04-26 18:05:33 +0300

    _WriterLayout eklendi

---
+++ MvcProjeKampi/Controllers/WriterPanelController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class WriterPanelController : Controller
+    {
+        // GET: WriterPanel
+
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+
+        public ActionResult WriterProfile()
+        {
+            return View();
+        }
+
+        public ActionResult MyHeading(int id)
+        {
+            id = 1;
+            var values = hm.GetListByWriter(id);
+            return View(values);
+        }
+    }
+}
